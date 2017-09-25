d4542e0510d9cdb46828a9ce97ebe0dffc0fce70
Balazs Sipocz <balazs.sipocz@me.com>
2017-09-25 01:13:59 -0700

    Fixed PEP8 missing line

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db.models import F
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -22,7 +23,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-        
+
     def increment_vote(self):
         self.votes = F('votes') + 1
         self.save()
