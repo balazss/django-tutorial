a7b09f160e09ffa30b21f39791b39d8323addd8e
Balazs Sipocz <balazs.sipocz@me.com>
2017-09-25 02:27:05 -0700

    Added some model tests and view tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -9,18 +10,24 @@ from .models import Choice, Question
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-    
+
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-        
-        
+        """
+        Return the last five published questions (not including those set to be
+        published in the future)
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.html'    
+    template_name = 'polls/results.html'
 
 
 def vote(request, pk):
@@ -30,8 +37,8 @@ def vote(request, pk):
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
         return render(request, 'polls/detail.html', {
-        'question': question,
-        'error_message': "You didn't select a choice!"
+            'question': question,
+            'error_message': "You didn't select a choice!"
         })
     else:
         selected_choice.increment_vote()
